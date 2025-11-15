a457177be928d416d938f5c5cfb084f2fea5f5fb
Vismaya V Nair <vijaysabi79@gmail.com>
2025-11-15 10:32:24 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,74 @@
+import streamlit as st
+import pickle
+import requests
+
+def fetch_poster(movie_id):
+    try:
+        url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=5f68a4826b57094667773645ae2b9268&language=en-US"
+        response = requests.get(url)
+
+        data = response.json()
+        poster_path = data.get("poster_path")
+
+        if poster_path:
+            return "https://image.tmdb.org/t/p/w500/" + poster_path
+        else:
+            return "https://via.placeholder.com/500x750?text=No+Image"
+
+    except:
+        return "https://via.placeholder.com/500x750?text=No+Image"
+
+
+# Load data
+movies = pickle.load(open('movies.pkl','rb'))
+movie_list = movies['title'].values
+
+similarity = pickle.load(open('similarity.pkl','rb'))
+
+
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+    movie_indices = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+    
+    recommended_movies = []
+    recommended_movies_posters = []
+
+    for i in movie_indices:
+        movie_id = movies.iloc[i[0]].movie_id     # ✅ FIXED movie_id
+        recommended_movies.append(movies.iloc[i[0]].title)
+        recommended_movies_posters.append(fetch_poster(movie_id))  # ✅ FIXED poster fetch
+    
+    return recommended_movies, recommended_movies_posters
+
+
+# Streamlit UI
+st.title('Movie Recommendation System')
+
+selected_movie = st.selectbox(
+    "Select a movie to get recommendations:",
+    movie_list
+)
+
+if st.button("Recommend Movies"):
+    names, posters = recommend(selected_movie) 
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.text(names[0])
+        st.image(posters[0])
+
+    with col2:
+        st.text(names[1])
+        st.image(posters[1])
+
+    with col3:
+        st.text(names[2])
+        st.image(posters[2])
+        
+    with col4:
+        st.text(names[3])
+        st.image(posters[3])
+    
+    with col5:
+        st.text(names[4])
+        st.image(posters[4])
